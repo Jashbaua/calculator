9f54238fb84fc5b9665460149acc9ef47914a51c
Jash <128972302+Jashbaua@users.noreply.github.com>
2024-08-31 00:08:32 +0530

    Add event listeners on all buttons

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let operand1=null,
     operand2=null,
     operator=null
+
 function add(num1,num2){
     return num1+num2
 }
@@ -11,6 +12,22 @@ function multiply(num1,num2){
     return num1*num2
 }
 function divide(num1,num2){
-    if(num2==0)return null
+    if(num2==0)return "div by 0"
     return num1/num2
+}
+function operate(operand1,operand2,operator){
+    switch(operator){
+        case '+':return add(operand1,operand2)
+        case '-':return subtract(operand1,operand2)
+        case '*':return multiply(operand1,operand2)
+        case '/':return divide(operand1,operand2)
+    }
+}
+
+let inpBtns=document.querySelectorAll('button')
+inpBtns.forEach((inpBtn)=>{
+    inpBtn.addEventListener('click',doButtonAction)
+})
+function doButtonAction(e){
+    console.log('pressd')
 }
