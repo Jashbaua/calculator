5f2b7dd143f977359cdbe43934ff479b6910e73b
Jash <128972302+Jashbaua@users.noreply.github.com>
2024-08-30 22:58:37 +0530

    Add operator functions in JS

---
+++ script.js
@@ -0,0 +1,16 @@
+let operand1=null,
+    operand2=null,
+    operator=null
+function add(num1,num2){
+    return num1+num2
+}
+function subtract(num1,num2){
+    return num1-num2
+}
+function multiply(num1,num2){
+    return num1*num2
+}
+function divide(num1,num2){
+    if(num2==0)return null
+    return num1/num2
+}
